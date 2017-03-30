cba2ff994865112295539a494b9ad8f89b9d4de3
Yuzhe Tang <tristartom@gmail.com>
2017-03-29 20:09:50 -0400

    t

--- marw5/main.c
+++ marw5/main.c
@@ -1,13 +1,8 @@
 #include<stdio.h> //printf
-#include<stdlib.h> //malloc
-//int array_data[] = {0,1,2};
 int main(){
   int array_stack[] = {0,1,2};
-  //int * array_heap = (int *)malloc(sizeof(int)*3);
-  //array_heap[0] = 1;array_heap[1] = 2;array_heap[2] = 3;
   int sum = 0;
-  int i;
-  for(i=0; i<=3; i++){
+  for(int i=0; i<=3; i++){
     sum += array_stack[i];
   }
   printf("sum = %d\n", sum);
